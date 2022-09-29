93b95bc85e4cb4c42b144933bcbca1640045f430
Wernnevon <wernnevon12@gmail.com>
2022-09-29 20:10:51 -0300

    hotfix: verify if patient id exists on create new patient

--- src/Infra/DAOarchive/patientDAO.ts
+++ src/Infra/DAOarchive/patientDAO.ts
@@ -1,22 +1,35 @@
 import Patient from "./model";
 import fs from "fs";
-import {LoadDB} from '../DB/db';
+import { LoadDB } from "../DB/db";
 
 const dbPath = "src/Infra/DB/db.json";
 
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  return "xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
 
+function checkId(id: string) {
+  const patientList = index();
+  const exists = patientList.some(
+    ({ personalData }: Patient) => personalData.id === id,
+  );
+  if (exists) {
+    console.log("id igual");
+    return uuidv4();
+  }
+  return id;
+}
+
 export function create(patient: Patient) {
   let db = LoadDB();
-  patient.id = uuidv4();
+  patient.personalData.id = checkId(uuidv4());
   db.push(patient);
   fs.writeFile(dbPath, JSON.stringify(db), function (err) {
-    if (err) throw err; 
+    if (err) throw err;
   });
 }
 
@@ -25,27 +38,29 @@ export function index() {
   return patients;
 }
 
-function findById(id: string | undefined){
+function findById(id: string | undefined) {
   const patients: Patient[] = LoadDB();
-  let paciente = patients.filter(patient => patient.id === id);
-  return paciente[0]
+  let paciente = patients.filter((patient) => patient.personalData.id === id);
+  return paciente[0];
 }
 
-export function update(patient: Patient){
+export function update(patient: Patient) {
   let patients: Patient[] = LoadDB();
-  let oldPatient = findById(patient.id);
+  let oldPatient = findById(patient.personalData.id);
   patients.splice(patients.indexOf(oldPatient), 1, patient);
   fs.writeFile(dbPath, JSON.stringify(patients), function (err) {
-    if (err) throw err; 
+    if (err) throw err;
   });
 }
 
-export function remove(patientId: string | undefined){
+export function remove(patientId: string | undefined) {
   let patients: Patient[] = LoadDB();
-  if(patientId){
-    patients = patients.filter((patient) => patient.id !== patientId);
+  if (patientId) {
+    patients = patients.filter(
+      (patient) => patient.personalData.id !== patientId,
+    );
     fs.writeFile(dbPath, JSON.stringify(patients), function (err) {
-      if (err) throw err; 
+      if (err) throw err;
     });
   }
   console.log(patients);
