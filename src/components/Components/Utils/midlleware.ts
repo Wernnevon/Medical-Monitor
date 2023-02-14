bc55ce40f44ed33e5428ccea1177a67fdcbb9031
Wernnevon <wernnevon12@gmail.com>
2023-02-14 14:18:10 -0300

    refactorProject

--- src/components/Components/Utils/midlleware.ts
+++
@@ -1,9 +0,0 @@
-export function validate(list:Array<any>){
-    if(list.length>0) return true;
-    return false;
-}
-
-export function patientExist(patientId: any){
-    if(patientId) return true;
-    return false;
-} 
