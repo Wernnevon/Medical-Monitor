45a5eb862d00fbcae07509589d430f64d471014e
Wernnevon <wernnevon12@gmail.com>
2024-04-26 12:03:21 -0300

    feat: detect click outside an component

---
+++ src/Hooks/useOutsideAlerter.tsx
@@ -0,0 +1,20 @@
+import { useEffect, MutableRefObject } from "react";
+
+/**
+ * Hook que executa uma ação quando clicado fora do compoente
+ */
+function useOutsideAlerter(ref: MutableRefObject<any>, action: any) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        if(action) action()
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [action, ref]);
+}
+
+export default useOutsideAlerter
