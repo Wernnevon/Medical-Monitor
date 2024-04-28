a7380867b3b7b3077da188a51b24fcb842f0895f
Wernnevon <wernnevon12@gmail.com>
2024-04-28 17:08:15 -0300

    feat: configurable filters and columns

--- src/Components/Table/index.tsx
+++ src/Components/Table/index.tsx
@@ -1,40 +1,119 @@
+/* eslint-disable no-useless-computed-key */
+import { HiUserGroup } from "react-icons/hi";
+import { IoPersonAdd } from "react-icons/io5";
 import KebabMenu from "../KebabMenu";
-import { Cell, Row, TableWrapper } from "./styles";
+import {
+  AddButton,
+  Cell,
+  Row,
+  TableWrapper,
+  TableHeader,
+  TitleWrapper,
+  FilterWrapper,
+  TableContainer,
+} from "./styles";
+import { RadioSelect, SearchTextFilter } from "../Filters";
 
-type Props = {
-  data: DataTable[];
+type Props<T> = {
+  columns: DataColumn[];
+  data: DataTable<T>[];
+  filters: DataFilter[];
+  config?: any;
 };
 
-type DataTable = {
-  id?: string;
+type DataTable<T> = {
+  [key: string]: T;
+};
+
+type DataFilter = {
+  type: "text" | "radio";
+  placeholder: string;
+  value?: Array<{ name: string; value: any }>;
+  handle: (v?: any) => void;
+};
+
+type DataColumn = {
   name: string;
-  city: string;
-  helthInsurance: string;
+  key: string;
+  type: string;
 };
 
-const Table: React.FC<Props> = ({ data }: Props) => {
+const Filters = {
+  ["text"]: ({ handle, placeholder }: DataFilter) => (
+    <SearchTextFilter
+      placeholder={placeholder}
+      onFind={handle}
+      key={placeholder}
+    />
+  ),
+  ["radio"]: ({ handle, placeholder, value }: DataFilter) => (
+    <RadioSelect
+      data={value ? value?.map(({ value }) => value) : []}
+      onSelect={handle}
+      placeholder={placeholder}
+      key={placeholder}
+    />
+  ),
+};
 
+const Table: React.FC<Props<any>> = ({
+  data,
+  columns,
+  filters,
+  config,
+}: Props<any>) => {
   return (
-    <TableWrapper>
-      <Row isHeader={true}>
-        <Cell>Paciente</Cell>
-        <Cell>Cidade</Cell>
-        <Cell>Convênio</Cell>
-        <Cell></Cell>
-      </Row>
-      {data.map((item, index) => (
-        <Row key={item.id} isOdd={index % 2 !== 0}>
-          <Cell align="left">{item.name}</Cell>
-          <Cell align="left">{item.city}</Cell>
-          <Cell align="left" isAction={true}>
-            {item.helthInsurance}
-          </Cell>
-          <Cell align="right" isAction={true}>
-            <KebabMenu />
-          </Cell>
+    <TableContainer>
+      <TableHeader>
+        <TitleWrapper>
+          <HiUserGroup
+            color="#03a696"
+            size={40}
+            style={{ position: "relative", bottom: ".15rem" }}
+          />
+          <label>Pacientes</label>
+        </TitleWrapper>
+        <FilterWrapper>
+          {filters.map((filter: DataFilter) => Filters[filter.type](filter))}
+          <AddButton>
+            <IoPersonAdd />
+            Novo
+          </AddButton>
+        </FilterWrapper>
+      </TableHeader>
+      <TableWrapper>
+        <Row isHeader={true}>
+          {columns &&
+            columns.map((column, i) => (
+              <Cell key={column.key + i}>{column.name}</Cell>
+            ))}
         </Row>
-      ))}
-    </TableWrapper>
+        {data.map((item, index) => (
+          <Row key={item.id} isOdd={index % 2 !== 0}>
+            {columns.map(({ key, type }, i) =>
+              type !== "action" ? (
+                <Cell
+                  key={key + i}
+                  widthCol={config.columnWidth[i]}
+                  align="left"
+                >
+                  {item[key]}
+                </Cell>
+              ) : (
+                <Cell
+                  key={key + i}
+                  widthCol={config.columnWidth[i]}
+                  align="right"
+                  isAction={true}
+                >
+                  <KebabMenu />
+                </Cell>
+              )
+            )}
+          </Row>
+        ))}
+      </TableWrapper>
+    </TableContainer>
   );
 };
 
--- src/Components/Table/styles.ts
+++ src/Components/Table/styles.ts
@@ -1,29 +1,89 @@
 import styled from "styled-components";
 
-export const TableWrapper = styled.div`
-  margin: 0 0 40px 0;
+export const TableContainer = styled.main`
+  display: flex;
+  flex-direction: column;
   width: 100%;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0px 3px 13px 5px #00000021;
+`;
+
+export const TableHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  margin: 1rem 0;
+  label {
+    font-family: "Akshar-Regular";
+  }
+`;
+
+export const FilterWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  height: 3rem;
+  gap: 1rem;
+`;
+
+
+export const AddButton = styled.button`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  border: none;
+  border-radius: 5em;
+  outline: none;
+  padding: 0.5rem 1.5rem;
+  background-color: #03a696;
+  color: #fff;
+  font-size: 1rem;
+  transition: 40ms ease-in;
+  font-family: "Akshar-Medium", sans-serif;
+  font-size: 1.3rem;
+  :hover {
+    cursor: pointer;
+  }
+  :active {
+    transform: translateY(2px);
+  }
+  @media (max-width: 1200px),
+    (max-width: 960px) and (-webkit-device-pixel-ratio: 1.25) {
+    height: 30px;
+    padding: 0;
+    font-size: 0.95rem;
+  }
+`;
+
+export const TableWrapper = styled.div`
+  box-sizing: border-box;
+  position: relative;
   display: table;
   text-align: left;
+  margin: 1rem;
 `;
 
 export const Row = styled.div<any>`
   display: table-row;
-  background-color: ${(props) =>
-    props.isHeader ? "#03a696" : props.isOdd ? "#f6f6f6" : "#fff"};
-  font-family: ${(props) => (props.isHeader ? "Akshar-Regular" : "Akshar-Light")};
-  color: ${(props) => (props.isHeader ? "#ffffff" : "#000000")};
-  font-size: ${({ isHeader }) => isHeader ? "1.8rem" : "1.5rem"};
 
-  div:first-child {
-    width: 50%;
-  }
+  background-color: ${({ isOdd, isHeader }) =>
+    isHeader ? "#e1e1e1" : isOdd ? "#f6f6f6" : "#fff"};
+  font-family: ${({ isHeader }) =>
+    isHeader ? "Akshar-Regular" : "Akshar-Light"};
+  color: #000;
+  font-size: ${({ isHeader }) => (isHeader ? "1.8rem" : "1.5rem")};
 `;
 
 export const Cell = styled.div<any>`
-  padding: .8rem 1.2rem;
+  padding: 0.8rem 1.2rem;
   display: table-cell;
   text-align: ${({ align }) => align};
   position: relative;
+  width: ${({ widthCol }) => widthCol};
 `;
