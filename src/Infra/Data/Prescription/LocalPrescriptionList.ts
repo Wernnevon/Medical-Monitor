2c21405af424ac7d2fde6dd8d4cbc3571739e68d
Wernnevon <wernnevon12@gmail.com>
2024-05-25 15:31:14 -0300

    feat: pass params to list per page

--- src/Infra/Data/Prescription/LocalPrescriptionList.ts
+++ src/Infra/Data/Prescription/LocalPrescriptionList.ts
@@ -8,8 +8,8 @@ class LocalPrescriptionList implements List {
     const response = await this.client.request({ method: "get", data: null });
     return response.data;
   }
-  async listPerPage(params: List.Params): Promise<Prescription[]> {
-    const response = await this.client.request({ method: "get", data: null });
+  async listPerPage(params: List.Params): Promise<any> {
+    const response = await this.client.request({ method: "get", data: params });
     return response.data;
   }
 }
