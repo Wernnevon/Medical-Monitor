67dfb0a5d38f9d95494687ed309c4213e4308a92
Wernnevon <wernnevon12@gmail.com>
2024-05-25 11:30:38 -0300

    feat: generic method top adapt to filter state on component

---
+++ src/Components/Utils/filterAdpater.ts
@@ -0,0 +1,26 @@
+import { List } from "../../Infra/Interfaces";
+
+type Props = {
+  keyFilter: string;
+  filterValue: any;
+  filterArray: List.Filter[];
+  callback(arg: any[]): void;
+};
+
+export function handleFilter({
+  keyFilter,
+  filterValue,
+  filterArray,
+  callback,
+}: Props) {
+  const filtersMap = new Map(filterArray.map(({ key, value }) => [key, value]));
+  if (filterValue) filtersMap.set(keyFilter, filterValue);
+  else filtersMap.delete(keyFilter);
+
+  const filtersArray = Array.from(filtersMap, ([key, value]) => ({
+    key,
+    value,
+  }));
+  console.log("aaaa");
+  if (callback) callback(filtersArray);
+}
