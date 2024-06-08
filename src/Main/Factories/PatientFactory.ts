cac3deb2d78f5cbe234fe0d52804f9f2f74dd767
Wernnevon <wernnevon12@gmail.com>
2024-06-08 12:01:09 -0300

    refactor: moved factories to main directory

---
+++ src/Main/Factories/PatientFactory.ts
@@ -0,0 +1,29 @@
+import {
+  LocalPatientStore,
+  LocalPatientUpdate,
+  LocalPatientDelete,
+  LocalPatientFind,
+  LocalPatientList,
+} from "../../Data";
+
+import makeClient from "./ClientFactory";
+
+const patientClient = makeClient("patients");
+
+const makeLocalPatientStore = () => new LocalPatientStore(patientClient);
+
+const makeLocalPatientUpdate = () => new LocalPatientUpdate(patientClient);
+
+const makeLocalPatientDelete = () => new LocalPatientDelete(patientClient);
+
+const makeLocalPatientFind = () => new LocalPatientFind(patientClient);
+
+const makeLocalPatientList = () => new LocalPatientList(patientClient);
+
+export {
+  makeLocalPatientStore,
+  makeLocalPatientUpdate,
+  makeLocalPatientDelete,
+  makeLocalPatientFind,
+  makeLocalPatientList,
+};
