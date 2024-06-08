7c07f16035215539bf5f3ec79ad5d5f44d9e4e99
Wernnevon <wernnevon12@gmail.com>
2024-06-08 12:02:10 -0300

    refactor: moved factories to main directory

--- src/Factories/ClientFactory.ts
+++
@@ -1,6 +0,0 @@
-import { IndexedDBClient } from "../Infra/Client";
-
-const makeClient = (storeName: string) =>
-  new IndexedDBClient({ dbName: "mmdb", dbVersion: 1, storeName });
-
-export default makeClient;
