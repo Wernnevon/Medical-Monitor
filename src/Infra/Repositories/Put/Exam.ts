8d9f0cb0e1240f4b545824670c6ec032370e083e
Wernnevon <wernnevon12@gmail.com>
2024-06-10 17:36:00 -0300

    feat: toggle exam status

--- src/Infra/Repositories/Put/Exam.ts
+++ src/Infra/Repositories/Put/Exam.ts
@@ -1,8 +1,10 @@
-import Exam from "../../../Domain/Entities/Exams";
+import Exam, { ExamStatus } from "../../../Domain/Entities/Exams";
+import { getStringToday } from "../../../Presentation/Utils/dateUtils";
 import {
   ConnectionType,
   getConnection,
 } from "../../Frameworks/indexedConnection";
+import { ExamRepository as GetExam } from "../Get";
 
 export class ExamRepository {
   async update(exam: Exam): Promise<void> {
@@ -21,5 +23,28 @@ export class ExamRepository {
     });
   }
 
-  changeStatus() {}
+  async changeStatus(id: number): Promise<void> {
+    const exam: any = await GetExam.prototype.findById(id);
+    exam.status =
+      exam.status === ExamStatus.DONE
+        ? ExamStatus.IN_PROGRESS
+        : ExamStatus.DONE;
+
+    if (exam.status === ExamStatus.IN_PROGRESS) delete exam.realizationDate;
+    else exam.realizationDate = getStringToday();
+
+    const db = await getConnection();
+    const transaction = db.transaction("exams", ConnectionType.READWRITE);
+    const objectStore = transaction.objectStore("exams");
+    const request = objectStore.put(exam);
+    return new Promise((resolve, reject) => {
+      request.onerror = () => {
+        reject(request.error);
+      };
+
+      request.onsuccess = () => {
+        resolve();
+      };
+    });
+  }
 }
