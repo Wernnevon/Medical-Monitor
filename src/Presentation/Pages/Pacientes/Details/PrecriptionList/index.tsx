e7926a8b86f9ab01f4396163f8ddf6f55561ae98
Wernnevon <wernnevon12@gmail.com>
2024-06-12 11:26:11 -0300

    fix: ensure data table update when user change data

--- src/Presentation/Pages/Pacientes/Details/PrecriptionList/index.tsx
+++ src/Presentation/Pages/Pacientes/Details/PrecriptionList/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { LuClipboardEdit, LuClipboardX } from "react-icons/lu";
 import { IoMdSwitch } from "react-icons/io";
@@ -57,17 +58,17 @@ export const PrescriptionList: React.FC<Props> = ({
   const filterPrescriptionTable: DataFilter[] = [
     {
       type: "radio",
-      placeholder: "Status",
+      placeholder: "Situação",
       handle: (filterValue: any) =>
         handleFilter({
-          keyFilter: "administering",
+          keyFilter: "status",
           filterValue,
           filterArray: filters,
           callback: setFilters,
         }),
       value: [
-        { name: "administering", value: PrescriptionStatus.ADMINISTERING },
-        { name: "administering", value: PrescriptionStatus.SUSPENDED },
+        { name: "status", value: PrescriptionStatus.ADMINISTERING },
+        { name: "status", value: PrescriptionStatus.SUSPENDED },
       ],
     },
     {
@@ -95,89 +96,92 @@ export const PrescriptionList: React.FC<Props> = ({
     {
       icon: <LuClipboardX />,
       name: "Deletar",
-      action: deleteExam,
+      action: deletePrescription,
     },
   ];
 
-  function deleteExam(examId: number) {
+  async function fetchPrescriptions() {
+    const response = await list.listPagination({
+      page: page,
+      pageSize,
+      filters,
+      keywords,
+    });
+    const { entries, totalEntries } = response;
+    const precriptionList: PrecriptionTableData[] = entries.map(
+      ({
+        id,
+        medicament: name,
+        date: requisitionDate,
+        status,
+      }: Prescription) => ({
+        id: id || 0,
+        name,
+        requisitionDate: formmatDate(requisitionDate),
+        status,
+      })
+    );
+    setPagination({
+      totalEntries: totalEntries || 0,
+      totalPages: Math.ceil(totalEntries / pageSize) || 0,
+    });
+    setPrescription(precriptionList || []);
+  }
+
+  async function deletePrescription(prescriptionId: number) {
     const popupData = {
       data: {
         title: "Excluir prescrição?",
         message: `Tem certeza de que deseja excluir? Não há como desfazer esta ação!`,
       },
-      onConfirm: () =>
-        remove
-          .delete({ ids: [examId] })
-          .then(() => {
-            addToast(
-              "A medicação foi apagada do histórico desse paciente",
-              ToastTypes.SUCESS
-            );
-          })
-          .catch(() => {
-            addToast(
-              "Não foi possível apagar a medicação do histórico deste paciente, tente novamente mais tarde",
-              ToastTypes.ERROR
-            );
-          }),
+      onConfirm: async () => {
+        try {
+          await remove.delete({ ids: [prescriptionId] });
+          addToast(
+            "A medicação foi apagada do histórico desse paciente",
+            ToastTypes.SUCESS
+          );
+          fetchPrescriptions();
+        } catch {
+          addToast(
+            "Não foi possível apagar a medicação do histórico deste paciente, tente novamente mais tarde",
+            ToastTypes.ERROR
+          );
+        }
+      },
     };
     showPopup(popupData);
   }
 
-  function changeStatus(examId: number) {
+  function changeStatus(prescriptionId: number) {
     const popupData = {
       data: {
         title: "Alterar situação?",
         message: `Tem certeza de que deseja alterar a situação?`,
       },
-      onConfirm: () =>
-        status
-          .changeStatus({ id: examId })
-          .then(() => {
-            addToast(
-              "A atual situação do do tratamento com farmaco foi atualizada no histórico desse paciente",
-              ToastTypes.SUCESS
-            );
-          })
-          .catch((err) => {
-            console.error(err);
-            addToast(
-              "Não foi possível mudar a atual situação do tratamento com farmaco no histórico deste paciente, tente novamente mais tarde",
-              ToastTypes.ERROR
-            );
-          }),
+      onConfirm: async () => {
+        try {
+          await status.changeStatus({ id: prescriptionId });
+          addToast(
+            "A atual situação do tratamento com farmaco foi atualizada no histórico desse paciente",
+            ToastTypes.SUCESS
+          );
+          fetchPrescriptions();
+        } catch (err) {
+          console.error(err);
+          addToast(
+            "Não foi possível mudar a atual situação do tratamento com farmaco no histórico deste paciente, tente novamente mais tarde",
+            ToastTypes.ERROR
+          );
+        }
+      },
     };
     showPopup(popupData);
   }
 
   useEffect(() => {
-    list
-      .listPagination({
-        page: page,
-        pageSize,
-        filters,
-        keywords,
-      })
-      .then(
-        ({ entries, totalEntries }: ListPagination.Response<Prescription>) => {
-          const list: PrecriptionTableData[] = entries.map(
-            ({ id, medicament: name, date: requisitionDate, status }) => ({
-              id: id || 0,
-              name,
-              requisitionDate: formmatDate(requisitionDate),
-              status,
-            })
-          );
-          setPagination((prev: any) => ({
-            ...prev,
-            totalEntries: totalEntries || 0,
-            totalPages: Math.ceil(totalEntries / pageSize) || 0,
-          }));
-          setPrescription(list || []);
-        }
-      );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filters, keywords, page, addToast]);
+    fetchPrescriptions();
+  }, [filters, keywords, page]);
 
   return (
     <Table
