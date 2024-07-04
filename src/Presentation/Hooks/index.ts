788a7803011a6493fb4d88072158d0e26a5936c2
Wernnevon <wernnevon12@gmail.com>
2024-07-04 17:33:18 -0300

    feat: exporting modal diagnosis and popup hooks from index

--- src/Presentation/Hooks/index.ts
+++ src/Presentation/Hooks/index.ts
@@ -1,6 +1,15 @@
+import { useModalDiagnosis } from "./useDiagnosis";
 import { useExame } from "./useExam";
 import useOutsideAlert from "./useOutsideAlert";
+import { usePopup } from "./usePopup";
 import { useRegister } from "./useRegister";
 import { useToast } from "./useToast";
 
-export { useExame, useOutsideAlert, useRegister, useToast };
+export {
+  useExame,
+  useOutsideAlert,
+  useRegister,
+  useToast,
+  useModalDiagnosis,
+  usePopup,
+};
